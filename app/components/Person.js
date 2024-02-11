657548fa59d724721cbfa417a7e59793a44f8442
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-02-11 04:24:06 -0500

    first commit

---
+++ app/components/Person.js
@@ -0,0 +1,80 @@
+"use client";
+
+import Image from "next/image";
+
+import { Crimson_Text } from "next/font/google";
+
+const crimson = Crimson_Text({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  weight: ["400", "600", "700"],
+});
+
+export default function Person({ name, role, image, listeningTo }) {
+  const rotation = Math.random() * 20 - 10;
+
+  return (
+    <div
+      className={`relative hover:h-52 group w-32 border-2 bg-white cursor-help px-2 py-2 rounded-md shadow-md hover:shadow-lg`}
+      style={{
+        transform: `rotate(${rotation}deg) scale(1)`,
+        transition: "transform 0.3s ease",
+        zIndex: 0,
+      }}
+      onMouseOver={(e) => {
+        e.currentTarget.style.transform = `rotate(${0}deg) scale(1.75)`;
+        e.currentTarget.style.zIndex = 1;
+      }}
+      onMouseOut={(e) => {
+        e.currentTarget.style.transform = `rotate(${rotation}deg) scale(1)`;
+        e.currentTarget.style.zIndex = 0;
+      }}
+    >
+      <Image
+        src={image}
+        alt="Person"
+        height={1000}
+        width={1000}
+        className="rounded-md w-full aspect-square shadow-sm"
+      />
+      <div className="flex flex-col -space-y-2">
+        <h1 className={`${crimson.className}  font-bold text-lg italic`}>
+          {name}
+        </h1>
+        <p className={`${crimson.className} font-bold text-slate-500 italic`}>
+          {role}
+        </p>
+      </div>
+      {/* <p
+        className={`${crimson.className} font-medium italic hidden group-hover:inline`}
+      >
+        Listening to:
+      </p>
+      <div className=" items-center gap-1 hidden group-hover:flex">
+        <Image
+          src={listeningTo.image}
+          alt="Person"
+          height={1000}
+          width={1000}
+          className="rounded-full w-4 h-4"
+        />
+        <p className="text-xs font-medium">{listeningTo.title}</p>
+      </div> */}
+      <div className="absolute bottom-0 left-0 right-0 p-2 hidden group-hover:block">
+        <p className={`${crimson.className} font-medium italic`}>
+          Listening to:
+        </p>
+        <div className="flex items-center gap-1">
+          <Image
+            src={listeningTo.image}
+            alt="Person"
+            height={1000}
+            width={1000}
+            className="rounded-full w-4 h-4"
+          />
+          <p className="text-xs font-medium">{listeningTo.title}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
