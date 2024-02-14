b66d77ac7f99522b29c23acc0a038e2fba5eb229
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-02-13 20:20:07 -0500

    Sort imports

--- app/page.js
+++ app/page.js
@@ -12,20 +12,20 @@ const crimson = Crimson_Text({
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="w-[33rem] mt-20 px-6">
-        <div className="flex gap-3 items-center">
+      <div className="mt-20 w-[33rem] px-6">
+        <div className="flex items-center gap-3">
           <Image
             src="/black.svg"
             alt="Kalisipudi Inc."
             width={5}
             height={5}
-            className="w-5 h-5"
+            className="h-5 w-5"
           />
           <h1
-            className={`text-2xl ${crimson.className} font-medium cursor-pointer`}
+            className={`text-2xl ${crimson.className} cursor-pointer font-medium`}
           >
             Monticello,{" "}
-            <span className="text-[#6C6C6C] hover:text-black transition">
+            <span className="text-[#6C6C6C] transition hover:text-black">
               a next-gen software company
             </span>
           </h1>
@@ -38,20 +38,20 @@ export default function Home() {
           business competitions, conducted hundreds of hours of research, and
           has spent a countless amount of time building amazing stuff.
         </p>
-        <h1 className={`text-3xl ${crimson.className} font-medium mt-5`}>
+        <h1 className={`text-3xl ${crimson.className} mt-5 font-medium`}>
           Projects
         </h1>
-        <h1 className={`text-3xl ${crimson.className} font-medium mt-5`}>
+        <h1 className={`text-3xl ${crimson.className} mt-5 font-medium`}>
           Team
         </h1>
-        <p className="text-sm text-[#4E4E4E] mt-2">
+        <p className="mt-2 text-sm text-[#4E4E4E]">
           We&apos;re a young team of builders, thinkers, and leaders, all
           focused toward building fast-moving, next-generation technology. Based
           out of Northern Virginia, our team has participated in dozens of
           business competitions, conducted hundreds of hours of research, and
           has spent a countless amount of time building amazing stuff.
         </p>
-        <div className="mt-10 grid sm:grid-cols-4 grid-cols-3  ">
+        <div className="mt-10 grid grid-cols-3 sm:grid-cols-4  ">
           {Team.map((person) => (
             <Person
               key={person.name}
@@ -62,7 +62,7 @@ export default function Home() {
             />
           ))}
         </div>
-        <div className={`mt-24 mb-8 italic ${crimson.className}`}>
+        <div className={`mb-8 mt-24 italic ${crimson.className}`}>
           <p className="text-center">
             Copyright ©2024 Kalisipudi Inc. All rights reserved.
           </p>
